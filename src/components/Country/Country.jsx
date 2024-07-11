d92ec11809f0abd66f6b607fb776fb9155a18d0d
IEEEMOSTAFA <skmostafa8888@gmail.com>
2024-07-11 10:49:54 +0600

    Mark visited and spread operator added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,22 +1,34 @@
 import { useState } from 'react';
 import './Country.css';
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     console.log(country);
     const { name, flags, population, area, cca3
     } = country;
     const [visited,setVisited] = useState(false);
+
+
+
     const handleVisited = () =>{
         setVisited(!visited);
     }
+
+    
+    
+    const passWithParams = () =>{
+        handleVisitedCountry(country);
+    }
+    // console.log(handleVisitedCountry);
     return (
-        <div className='country'>
-            <h3>Name: {name.common}</h3>
+        <div className={`country ${visited && 'visited'}`}>
+            <h3 style={{color: visited ? 'Purple': 'Black'}}>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area :{area}</p>
             <p><small>Code : {cca3
             }</small></p>
-            <button onClick={handleVisited}>{visited ? 'visited': 'Going'}</button>
+            <button style={{background: 'yellow', color:'black'}} onClick={passWithParams}>Mark Visited</button>
+            <br />
+            <button onClick={handleVisited}>{visited ? 'visited': 'non-visited'}</button>
             {/* {visited && 'I have visited this country.'} */}
             {visited ? 'I have visited this country.' : 'I want to visit'}
         </div>
